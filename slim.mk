bf375a8d5e5bc97638aea73785d1c7fa37d6237a
Kasey Jones <k_jones_ford@hotmail.com>
2014-09-10 14:01:24 -0400

    Aokp gee bring up

--- slim.mk
+++ slim.mk
@@ -1,34 +1,21 @@
-#
-# Copyright 2013 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Inherit AOSP device configuration for gee
+$(call inherit-product, device/lge/gee/full_gee.mk)
 
-# Get the long list of APNs
-$(call inherit-product-if-exists, vendor/slim/config/gsm.mk)
+# Inherit AOKP common bits
+$(call inherit-product, vendor/aokp/configs/common.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/slim/config/common.mk)
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Mako Overlay
+#PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/mako
 
-PRODUCT_NAME := slim_gee
-PRODUCT_DEVICE := gee
+# Setup device specific product configuration
+PRODUCT_NAME := aokp_gee
 PRODUCT_BRAND := Android
+PRODUCT_DEVICE := gee
 PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := false
 
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/lge/gee/device.mk)
-$(call inherit-product-if-exists, vendor/lge/gee/gee-vendor.mk)
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_768.zip:system/media/bootanimation.zip
